88488dd80843fad26b44d76bb29f753cc3a1d198
Tereza Okalova <okalova.t@gmail.com>
2025-02-25 22:38:33 -0500

    accidentally not taking all electrodes

--- notebooks/anphy_code/process_all.py
+++ notebooks/anphy_code/process_all.py
@@ -11,8 +11,19 @@ for subject in subject_dirs:
         cmd = f"python process_single_subject_group.py {subject} {group_index}"
         print(f"  Running command: {cmd}")
         try:
-            # launches a separate Python process for each batch
             subprocess.run(cmd, shell=True, check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error processing {subject} group {group_index}: {e}")
+
+
+# for subject in subject_dirs:
+#     print(f"Processing subject: {subject}")
+#     for group_index in range(10):
+#         cmd = f"python process_single_subject_group.py {subject} {group_index}"
+#         print(f"  Running command: {cmd}")
+#         try:
+#             # launches a separate Python process for each batch
+#             subprocess.run(cmd, shell=True, check=True)
+#         except subprocess.CalledProcessError as e:
+#             print(f"Error processing {subject} group {group_index}: {e}")
 
