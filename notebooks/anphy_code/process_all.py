d4f7d3fb2c7b0a11f382f7e8f61a7d44d2c69b46
Tereza Okalova <okalova.t@gmail.com>
2025-02-28 10:39:29 -0500

    donegit add process_all.py process_single_subject_group.pygit add process_all.py process_single_subject_group.py

--- notebooks/anphy_code/process_all.py
+++ notebooks/anphy_code/process_all.py
@@ -5,7 +5,10 @@ data_dir = DataConfig.BASE_PATH
 subject_dirs = sorted([d for d in os.listdir(data_dir)
                        if d.lower().startswith("epctl") and os.path.isdir(os.path.join(data_dir, d))])
 
-for subject in subject_dirs:
+subject_subset = list(subject_dirs[7:9]) + list(subject_dirs[20:]) # added
+
+# modif for the remaining ones
+for subject in subject_subset:
     print(f"Processing subject: {subject}")
     for group_index in range(10):
         cmd = f"python process_single_subject_group.py {subject} {group_index}"
@@ -15,15 +18,12 @@ for subject in subject_dirs:
         except subprocess.CalledProcessError as e:
             print(f"Error processing {subject} group {group_index}: {e}")
 
-
 # for subject in subject_dirs:
-#     print(f"Processing subject: {subject}")
-#     for group_index in range(10):
-#         cmd = f"python process_single_subject_group.py {subject} {group_index}"
-#         print(f"  Running command: {cmd}")
-#         try:
-#             # launches a separate Python process for each batch
-#             subprocess.run(cmd, shell=True, check=True)
-#         except subprocess.CalledProcessError as e:
-#             print(f"Error processing {subject} group {group_index}: {e}")
-
+    # print(f"Processing subject: {subject}")
+    # for group_index in range(10):
+    #     cmd = f"python process_single_subject_group.py {subject} {group_index}"
+    #     print(f"  Running command: {cmd}")
+    #     try:
+    #         subprocess.run(cmd, shell=True, check=True)
+    #     except subprocess.CalledProcessError as e:
+    #         print(f"Error processing {subject} group {group_index}: {e}")
